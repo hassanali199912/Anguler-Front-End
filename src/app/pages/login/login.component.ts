0bb52ae690d6d1e3b7dba2f92a2b624eb0ae856a
davzekry <37640599+davzekry@users.noreply.github.com>
2025-05-26 16:45:40 +0300

    after editing the login component and service

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -18,30 +18,74 @@ export class LoginComponent {
     password: new FormControl("Hh@12345678910", [Validators.required, Validators.minLength(8)]),
   });
 
-  constructor(private authService: AuthService, private router: Router) {
-  }
-  onSubmit() {
-    if (this.LoginFrom.valid) {
-      this.authService.SetLoginForm(this.LoginFrom.value).subscribe({
-        next: (res: LoginResponse) => {
-          const token = res?.data?.token;
-          if (token) {
-            localStorage.setItem('token', token);
-            this.router.navigate(['/main']);
-          } else {
-            alert('Login failed: token not found');
-          }
+  isLoading = false;
+  msgSuccess = false;
+  errorMsg = '';
 
+  constructor(private _authService: AuthService, private router: Router) {
+  }
+
+  
+  onSubmit() {
+    this.isLoading = true;
+    this.errorMsg = '';
+    if (this.LoginFrom.valid) {
+      this._authService.SetLoginForm(this.LoginFrom.value).subscribe({
+        next: (res) => {
+          console.log(res);
+          if (res.status === true && res.data && res.data.token) {
+            this.msgSuccess = true;
+            setTimeout(() => {
+              // 1 - Save Token
+              localStorage.setItem('userToken', res.data.token);
+
+              // 2 - Decode and save user data
+              this._authService.saveUserData();
+
+              // 3 - Navigate to home
+              this.router.navigate(['/main']);
+            }, 1000);
+          } else {
+            // Handle failure
+            this.errorMsg = res.massage || 'Login failed!';
+            alert(this.errorMsg);
+          }
+          this.isLoading = false;
         },
         error: (error) => {
-          alert('Login failed!');
+          this.isLoading = false;
+          this.errorMsg = 'Login failed!';
+          alert(this.errorMsg);
           console.error(error);
-
         }
-      })
-
+      });
+    } else {
+      this.isLoading = false;
     }
   }
+
+  // onSubmit() {
+  //   if (this.LoginFrom.valid) {
+  //     this.authService.SetLoginForm(this.LoginFrom.value).subscribe({
+  //       next: (res: LoginResponse) => {
+  //         const token = res?.data?.token;
+  //         if (token) {
+  //           localStorage.setItem('token', token);
+  //           this.router.navigate(['/main']);
+  //         } else {
+  //           alert('Login failed: token not found');
+  //         }
+
+  //       },
+  //       error: (error) => {
+  //         alert('Login failed!');
+  //         console.error(error);
+
+  //       }
+  //     })
+
+    }
+  
 
   // onSubmit() {
   //   if (this.loginForm.valid) {
@@ -62,4 +106,4 @@ export class LoginComponent {
   //     });
   //   }
   // }
-}
+
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../environment/environment';
+import { jwtDecode } from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,7 @@ import { environment } from '../environment/environment';
 export class AuthService {
 
   private readonly _HttpClient = inject(HttpClient);
+  userData: any;
 
   SetRegisterForm(data: object): Observable<any> {
     return this._HttpClient.post(`${environment.baseURL}/api/Auth/register`, data);
@@ -16,5 +18,12 @@ export class AuthService {
 
   SetLoginForm(data: object): Observable<any> {
     return this._HttpClient.post(`${environment.baseURL}/api/Auth/login`, data);
+  }
+
+  saveUserData(): void {
+    if (localStorage.getItem('userToken') !== null) {
+      this.userData = jwtDecode(localStorage.getItem('userToken')!);
+      console.log('userData', this.userData);
+    }
   }
 }
